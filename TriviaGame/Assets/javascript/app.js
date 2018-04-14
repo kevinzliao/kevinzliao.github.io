2dcc42b673e18695f1d1d41462776453f4ec0e8b
kevinzliao <kevinzliao@gmail.com>
2018-04-14 02:27:35 -0500

    Add files via upload

---
+++ TriviaGame/Assets/javascript/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    
+    var right = 0;
+    var wrong = 0;
+    var answered = 0;
+    var questions = 0;
+ 
+    var time = 30;
+    var myVar = setInterval(function() { 
+        myTimer()}, 1000);
+        
+    $(".button").click(endGame);
+
+    function endGame() {  
+        clearInterval(myVar);
+
+        //finds total number of questions
+        $(".question").each(function() {             
+            questions++;
+        });
+
+        //check 
+        $(".question :checked").each(function() {  
+            answered++;
+            if ($(this).val() == "correct") {
+                right++;
+            }
+            else if ($(this).val() == "wrong") {
+                wrong++;
+            }
+        });
+        unanswered = questions - answered;
+
+        $("#questions").empty();  
+        $("#questions").append("<h1>Correct Answers: "+ right);  
+        $("#questions").append("<h1>Wrong Answers: "+ wrong);    
+        $("#questions").append("<h1>Unanswered: "+ unanswered);      
+    }
+    
+    function myTimer() {
+        time--;
+        $("#time").text(time);
+        
+
+        if (time == 0) {
+            alert("Times up!");
+            endGame();
+        }
+
+    }
+
+ });
---
+++ TriviaGame/README.md
@@ -0,0 +1,1 @@
+Try to answer as many questions as you can in 30 seconds!
